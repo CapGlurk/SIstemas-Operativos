99e4e4d76d56291df39d9047359ed6cc3514806b
CapGlurk <pedro1990galvis@gmail.com>
2019-05-19 21:42:30 -0500

    primero0

---
+++ js/cod.js
@@ -0,0 +1,10 @@
+  
+        
+        function AbrirForm() {
+  document.getElementById("formu").style.display = "block";
+}
+
+function cerrarForm() {
+  document.getElementById("formu").style.display = "none";
+}
+     
